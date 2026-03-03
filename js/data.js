30c1d0a756c1864aada6ae3df2a94eddd9daa841
Rohan Nigam <rohannigam3108@gmail.com>
2026-03-03 23:24:15 +0530

    Initial commit - CineWorld Movie App

---
+++ js/data.js
@@ -0,0 +1,20 @@
+let movies = [
+    {
+        image: 'img/banner/banner1.webp'
+    },
+    {
+        image: 'img/banner/banner2.webp'
+    },
+    {
+        image: 'img/banner/banner3.webp'
+    },
+    {
+        image: 'img/banner/banner4.webp'
+    },
+    {
+        image: 'img/banner/p8.png'
+    },
+    {
+        image: 'img/banner/banner6.webp'
+    }
+]
